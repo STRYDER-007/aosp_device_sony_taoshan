2dd0ceb089a93654b424322f73cc8f228747b9ab
Mahesh Mahimane <mahesh140494@gmail.com>
2017-04-09 17:40:27 +0000

    taoshan: Build Camera2

--- product/camera.mk
+++ product/camera.mk
@@ -1,8 +1,8 @@
 # Camera packages
 PRODUCT_PACKAGES += \
     camera.qcom \
-    libstlport
-
+    libstlport \
+    Camera2
 # Camera properties
 PRODUCT_PROPERTY_OVERRIDES += \
     camera2.portability.force_api=1
